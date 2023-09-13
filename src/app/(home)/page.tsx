f1bc9eba02e5bb706a5e8296eaafe9f180ed1a6d
Franco Martin Paturlanne <paturlannefm@gmail.com>
2023-09-12 23:24:26 -0300

    Initialized project with folder structure and Sass integration

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import variables from "../variables.module.scss";
+import "../styles/styles.scss";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <p style={{ color: variables.primaryColor }}>My portfolio</p>
+      </div>
+    </main>
+  );
+}
