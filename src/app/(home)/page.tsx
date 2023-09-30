e45e9b91693eb9563a65737291816ad07ee3043d
Franco Martin Paturlanne <paturlannefm@gmail.com>
2023-09-30 17:30:44 -0300

    Implemented navbar component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,24 @@
 import variables from "../variables.module.scss";
 import "../styles/styles.scss";
 
+import Cover from "../components/Cover";
+import NavbarComponent from "../components/Navbar";
+
 export default function Home() {
   return (
     <main>
       <div>
         <p style={{ color: variables.primaryColor }}>My portfolio</p>
+        <NavbarComponent />
+        <Cover />
+        {/*
+      <Navbar/>
+      <Cover/>
+      <About/>
+      <WorkExperience/>
+      <Projects/>
+      <Contact/>
+      */}
       </div>
     </main>
   );
---
+++ src/app/components/Cover.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+function Cover() {
+  return <div className="bg-div"></div>;
+}
+
+export default Cover;
